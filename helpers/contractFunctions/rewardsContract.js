22c8991b5f37c78de3058d3c6c16a2699e5ba1c8
eratos <eratos1122@gmail.com>
2021-04-11 18:14:53 +0800

    Hotfix - Zap & Stake

--- helpers/contractFunctions/rewardsContract.js
+++ helpers/contractFunctions/rewardsContract.js
@@ -1,22 +1,31 @@
 /* eslint-disable */
-import { rewards } from '@/config/contractConfig'
-import { sendTransaction as _sendTransaction, makeBatchCall as _makeBatchCall } from "./base";
+import { rewards } from "@/config/contractConfig";
+import {
+  sendTransaction as _sendTransaction,
+  makeBatchCall as _makeBatchCall,
+  getNetworkId
+} from "./base";
 
-let contractInstance
+let contractInstance;
 
 const initContractInstance = () => {
-  if (!contractInstance || web3.currentProvider.isMetaMask !== contractInstance.currentProvider.isMetaMask) {
-    contractInstance = new web3.eth.Contract(rewards.abi, rewards.address[3])
+  if (
+    !contractInstance ||
+    web3.currentProvider.isMetaMask !==
+      contractInstance.currentProvider.isMetaMask
+  ) {
+    const address = rewards.address[getNetworkId()];
+    contractInstance = new web3.eth.Contract(rewards.abi, address);
   }
-}
+};
 
 export const sendTransaction = async (methodName, args, options) => {
   initContractInstance();
-  const method = contractInstance.methods[methodName](...args)
-  return await _sendTransaction(method, options)
-}
+  const method = contractInstance.methods[methodName](...args);
+  return await _sendTransaction(method, options);
+};
 
-export const makeBatchCall = async (methods) => {
+export const makeBatchCall = async methods => {
   initContractInstance();
-  return await _makeBatchCall(contractInstance, methods)
-}
+  return await _makeBatchCall(contractInstance, methods);
+};
--- store/ethereum.js
+++ store/ethereum.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import { enableAccount, isRightNetwork } from '@/helpers/web3Provider'
-import networkConfig from '@/config/networkConfig'
+import { enableAccount, isRightNetwork } from "@/helpers/web3Provider";
+import networkConfig from "@/config/networkConfig";
 
 export const state = () => ({
   loading: true,
@@ -13,120 +13,127 @@ export const state = () => ({
     root: null,
     address: null,
     tx: null
-  },
-})
+  }
+});
 
 export const mutations = {
   INIT_ETH_DATA: (state, payload) => {
-    const web3Copy = state
-    web3Copy.initialized = payload.initialized
-    web3Copy.account = payload.account
+    const web3Copy = state;
+    web3Copy.initialized = payload.initialized;
+    web3Copy.account = payload.account;
     if (payload.networkId) {
-      web3Copy.networkId = parseInt(payload.networkId)
+      web3Copy.networkId = parseInt(payload.networkId);
     }
-    state = web3Copy
+    state = web3Copy;
   },
   SET_ACCOUNT: (state, account) => {
-    state.account = account
+    state.account = account;
   },
   SET_NETWORK: (state, networkId) => {
-    state.networkId = parseInt(networkId)
+    state.networkId = parseInt(networkId);
   },
   SET_LOADING: (state, loading) => {
-    state.loading = loading
+    state.loading = loading;
   },
   SET_EXPLORER: (state, payload) => {
-    state.explorer = payload
+    state.explorer = payload;
   }
-}
+};
 
 export const actions = {
   initialize({ commit, state }, payload) {
-    commit('INIT_ETH_DATA', payload)
-    commit('SET_LOADING', false)
+    commit("INIT_ETH_DATA", payload);
+    commit("SET_LOADING", false);
 
     if (isRightNetwork(state.networkId)) {
-      commit('SET_EXPLORER', networkConfig[state.networkId].explorer)
+      commit("SET_EXPLORER", networkConfig[state.networkId].explorer);
     } else {
-      commit('SET_EXPLORER', networkConfig[state.defaultNetworkId].explorer)
+      commit("SET_EXPLORER", networkConfig[state.defaultNetworkId].explorer);
     }
   },
 
   async connectAccount({ commit, state }) {
     if (!state.account) {
-      const account = await enableAccount()
-      commit('SET_ACCOUNT', account)
-      return true
+      const account = await enableAccount();
+      commit("SET_ACCOUNT", account);
+      return true;
     } else {
-      return false
+      return false;
     }
   },
 
-  setAccount ({ commit }, payload) {
-    commit('SET_ACCOUNT', payload)
+  setAccount({ commit }, payload) {
+    commit("SET_ACCOUNT", payload);
   },
 
-  setNetwork ({ commit }, networkId) {
-    commit('SET_NETWORK', networkId)
+  setNetwork({ commit }, networkId) {
+    commit("SET_NETWORK", networkId);
   },
 
-  setExplorer ({ commit }, payload) {
-    commit('SET_EXPLORER', payload)
+  setExplorer({ commit }, payload) {
+    commit("SET_EXPLORER", payload);
   }
-}
+};
 
 export const getters = {
-  isOk: (state) => {
-    return !!(isRightNetwork(state.networkId) && state.account && state.isInjected)
+  isOk: state => {
+    return !!(
+      isRightNetwork(state.networkId) &&
+      state.account &&
+      state.isInjected
+    );
   },
 
-  isRightNetwork: (state) => {
-    // return isRightNetwork(state.networkId)
-    return state.networkId === 3
+  isRightNetwork: state => {
+    return isRightNetwork(state.networkId);
+    // return state.networkId === 3
   },
 
-  initialized: (state) => {
-    return state.initialized
+  initialized: state => {
+    return state.initialized;
   },
 
-  rightNetwork: (state) => {
-    return state.rightNetwork
+  rightNetwork: state => {
+    return state.rightNetwork;
   },
 
-  networkId: (state) => {
-    return state.networkId
+  networkId: state => {
+    return state.networkId;
   },
 
-  defaultNetworkId: (state) => {
-    return state.defaultNetworkId
+  defaultNetworkId: state => {
+    return state.defaultNetworkId;
   },
 
-  currentProvidersNetworkId: (state) => {
+  currentProvidersNetworkId: state => {
     if (isRightNetwork(state.networkId)) {
-      return state.networkId
+      return state.networkId;
     } else {
-      return state.defaultNetworkId
+      return state.defaultNetworkId;
     }
   },
 
-  account: (state) => {
-    return state.account
+  account: state => {
+    return state.account;
   },
 
-  accountConnected: (state) => {
-    return state.account !== '' && state.account !== undefined && state.account !== null;
+  accountConnected: state => {
+    return (
+      state.account !== "" &&
+      state.account !== undefined &&
+      state.account !== null
+    );
   },
 
-  explorer: (state) => {
-    return state.explorer
+  explorer: state => {
+    return state.explorer;
   },
 
-  txUrl: (state) => {
-    return state.explorer.root + state.explorer.tx
+  txUrl: state => {
+    return state.explorer.root + state.explorer.tx;
   },
 
-  loading: (state) => {
-    return state.loading
-  },
-
-}
+  loading: state => {
+    return state.loading;
+  }
+};
